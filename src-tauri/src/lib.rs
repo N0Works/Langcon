4aba96732f21b251456dd8079364d43e9a85cb4c
0326 <ahhgo326@gmail.com>
2025-12-07 06:34:05 +0900

    v0.1.1 update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -356,6 +356,9 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            show_main_window(app);
+        }))
         .on_window_event(|window, event| {
             if window.label() != "main" {
                 return;
